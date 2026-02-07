a2d9a767d067414062cbf64304bb773d33f57b8b
Zirui Cai <zirui.cai@swanhub.co>
2026-02-07 13:18:11 +0800

    refactor: update input monitoring logging to use event name

--- src-tauri/src/core/input_monitor.rs
+++ src-tauri/src/core/input_monitor.rs
@@ -1,6 +1,5 @@
-use rdev::{listen, Event, EventType, Key, Button};
+use rdev::{listen, Event, EventType};
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
 use std::thread;
 use log::{info, error};
 
@@ -37,24 +36,24 @@ fn callback(event: Event) {
     
     match event.event_type {
         EventType::KeyPress(key) => {
-            info!("键盘按下: {:?} at ({}, {})", key, event.position.x, event.position.y);
+            info!("键盘按下: {:?}, event name: {:?}", key, event.name);
         }
         EventType::KeyRelease(key) => {
-            info!("键盘释放: {:?} at ({}, {})", key, event.position.x, event.position.y);
+            info!("键盘释放: {:?}, event name: {:?}", key, event.name);
         }
         EventType::ButtonPress(button) => {
-            info!("鼠标按下: {:?} at ({}, {})", button, event.position.x, event.position.y);
+            info!("鼠标按下: {:?}, event name: {:?}", button, event.name);
         }
         EventType::ButtonRelease(button) => {
-            info!("鼠标释放: {:?} at ({}, {})", button, event.position.x, event.position.y);
+            info!("鼠标释放: {:?}, event name: {:?}", button, event.name);
         }
         EventType::MouseMove { x, y } => {
             // 鼠标移动事件太频繁，可以选择性记录
             // info!("鼠标移动到: ({}, {})", x, y);
         }
         EventType::Wheel { delta_x, delta_y } => {
-            info!("鼠标滚轮: delta_x={}, delta_y={} at ({}, {})", 
-                  delta_x, delta_y, event.position.x, event.position.y);
+            info!("鼠标滚轮: delta_x={}, delta_y={}", 
+                  delta_x, delta_y);
         }
     }
 }
