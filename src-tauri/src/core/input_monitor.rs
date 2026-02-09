fddc14ee3291478bee56aea28fd066b2f843d7d3
Zirui Cai <zirui.cai@swanhub.co>
2026-02-09 10:22:50 +0800

    refactor: remove unused Arc import in input_monitor module

--- src-tauri/src/core/input_monitor.rs
+++ src-tauri/src/core/input_monitor.rs
@@ -1,5 +1,4 @@
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
 use tokio::process::{Child, Command};
 use tokio::io::{AsyncBufReadExt, BufReader};
 use log::{info, error, warn};
