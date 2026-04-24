c1d0acb9dd21244d843f10fb8948b0711695d69b
Zirui Cai <zirui.cai@swanhub.co>
2026-04-24 10:03:09 +0800

    opt: single protect

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -45,6 +45,9 @@ pub fn init_and_run() {
     let app = tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            show_main_window(app);
+        }))
         .manage(server_state)
         .invoke_handler(server::router::handler())
         .setup(|app| {
